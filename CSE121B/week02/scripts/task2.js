2ca07a51a361d74998dd831dd76aee22d05fdee0
letiabelenda <l_abelenda@hotmail.com>
2023-01-10 22:34:32 -0300

    variables task2 done

--- CSE121B/week02/scripts/task2.js
+++ CSE121B/week02/scripts/task2.js
@@ -3,56 +3,54 @@
 /* VARIABLES */
 
 // Step 1: declare and instantiate a variable to hold your name
-let myName = "Leticia";
+let myName = "Leticia Abelenda";
 
 // Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
-let theNameElement = document.getElementById('name');
-theNameElement.querySelector('#name').innerHTML = 'Leticia';
+document.querySelector('#name').textContent = myName;
 
 // Step 3: declare and instantiate a variable to hold the current year
 let currentYear = "2023";
 
 // Step 4: place the value of the current year variable into the HTML file
-//document.querySelector('#year');
+document.querySelector('#year').textContent = currentYear;
 
 // Step 5: declare and instantiate a variable to hold the name of your picture
-let myPicture = "me"
-
+let myPicture = "images/me.jpg"
 // Step 6: copy your image into the "images" folder
 
 
 // Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
-document.querySelector('#mainImage').src = 'images/me.png';
+document.querySelector('img').setAttribute('src', myPicture);
 
 
 
 /* ARRAYS */
 
 // Step 1: declare and instantiate an array variable to hold your favorite foods
-const foods = ["Lamb", "Sushi"];
+//const foods = ["Lamb", "Sushi"];
 
 // Step 2: place the values of the favorite foods variable into the HTML file
-document.querySelector('#food');
+//document.querySelector('#food');
 
 // Step 3: declare and instantiate a variable to hold another favorite food
-let otherFood = "Ramen";
+//let otherFood = "Ramen";
 
 // Step 4: add the variable holding another favorite food to the favorite food array
-foods.push(otherFood);
+//foods.push(otherFood);
 
 // Step 5: repeat Step 2
-document.querySelector('#food');
+//document.querySelector('#food');
 
 // Step 6: remove the first element in the favorite foods array
-foods.shift();
+//foods.shift();
 
 // Step 7: repeat Step 2
-document.querySelector('#food');
+//document.querySelector('#food');
 
 // Step 8: remove the last element in the favorite foods array
-foods.pop();
+//foods.pop();
 
 // Step 7: repeat Step 2
-document.querySelector('#food');
+//document.querySelector('#food');
 
 
