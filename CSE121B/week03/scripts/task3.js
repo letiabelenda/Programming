88cb5bd52b66310609b952646f6589640016ea69
letiabelenda <l_abelenda@hotmail.com>
2023-01-20 20:21:52 -0300

    task 3 - functions done

--- CSE121B/week03/scripts/task3.js
+++ CSE121B/week03/scripts/task3.js
@@ -6,28 +6,51 @@
 function add(number1, number2) {
 
 // Step 2: In the function, return the sum of the parameters number1 and number2
-    return (number1 + number2);
+    return parseFloat(number1 + number2);
 }
 // Step 3: Step 3: Using function declaration, define another function named addNumbers that gets the values of two HTML form controls with IDs of addend1 and addend2. Pass them to the add function
 function addNumbers() {
-    let addend1 = document.querySelector("#addend1").value;
-    console.log(addend1);
-    let addend2 = document.querySelector("#addend2").value;
-    console.log(addend2);
+    let addend1 = parseFloat(document.getElementById("addend1").value);
+    let addend2 = parseFloat(document.getElementById("addend2").value);
 
-    let result = add(addend1, addend2);
+    let sum = add(addend1, addend2);
 
 // Step 4: Assign the return value to an HTML form element with an ID of sum
-    let sum = document.querySelector("#sum");
-    sum.innerHTML = result;
+    document.getElementById("sum").value = sum;
 }
+
 // Step 5: Add a "click" event listener to the HTML button with an ID of addNumbers that calls the addNumbers function
-const buttonElement = document.querySelector("#addNumbers");
-buttonElement.addEventListener("click", addNumbers);
+document.getElementById("addNumbers").addEventListener("click", addNumbers);
 
 // Step 6: Using function expressions, repeat Steps 1-5 with new functions named subtract and subtractNumbers and HTML form controls with IDs of minuend, subtrahend, difference and subtractNumbers
+function subtract(number1, number2) {
+    return parseFloat(number1 - number2);
+}
+
+function subtractNumbers() {
+    let minuend = parseFloat(document.getElementById("minuend").value);
+    let subtrahend = parseFloat(document.getElementById("subtrahend").value);
+
+    let difference = subtract(minuend, subtrahend);
+
+    document.getElementById("difference").value = difference;
+}
+document.getElementById("subtractNumbers").addEventListener("click", subtractNumbers);
 
 // Step 7: Using arrow functions, repeat Steps 1-5 with new functions named multiply and mulitplyNumbers and HTML form controls with IDs of factor1, factor2, product and multiplyNumbers
+function multiply (number1, number2) {
+    return parseFloat(number1 * number2);
+}
+
+function multiplyNumbers () {
+    let factor1 = parseFloat(document.getElementById("factor1").value);
+    let factor2 = parseFloat(document.getElementById("factor2").value);
+
+    let product = multiply(factor1, factor2);
+
+    document.getElementById("product").value = product;
+}
+document.getElementById("multiplyNumbers").addEventListener("click", multiplyNumbers);
 
 // Step 8: Using any of the three function declaration types, repeat Steps 1-5 with new functions named divide and divideNumbers and HTML form controls with IDs of dividend, divisor, quotient and divideNumbers
 
@@ -37,12 +60,16 @@ buttonElement.addEventListener("click", addNumbers);
 /* BUILT-IN METHODS */
 
 // Step 1: Declare and instantiate a variable of type Date to hold the current date
+let date = new Date;
 
 // Step 2: Declare a variable to hold the current year
+let year = "";
 
 // Step 3: Using the variable declared in Step 1, call the built-in getFullYear() method/function and assign it to the variable declared in Step 2
+year = date.getFullYear();
 
 // Step 4: Assign the current year variable to an HTML form element with an ID of year
+document.getElementById("year").innerHTML = year;
 
 
 /* ARRAY METHODS */
