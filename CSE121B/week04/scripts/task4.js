e8bfd99a89cc445bd7391f085579436c50e78631
letiabelenda <l_abelenda@hotmail.com>
2023-01-24 16:31:26 -0300

    task4 data completed

--- CSE121B/week04/scripts/task4.js
+++ CSE121B/week04/scripts/task4.js
@@ -3,22 +3,36 @@
 /* DATA */
 
 // Step 1: Declare a new variable to hold information about yourself
-
+const myInformation = {
 // Step 2: Inside of the object, add a property named name with a value of your name as a string
-
+    name: "Leticia Abelenda",
+    
 // Step 3: Add another property named photo with a value of the image path and name (used in Task 2) as a string
+    photo: "images/Leti.jpg",
 
 // Step 4: Add another property named favoriteFoods with a value of an array of your favorite foods as strings ( hint: [] )
+    favoriteFoods: ["Ramen", "Lamb", "Sushi", "Pizza"],
 
 // Step 5: Add another property named hobbies with a value of an array of your hobbies as strings
+    hobbies: ["Reading", "Drawing", "Playing videogames", "Watching movies and series"],
 
 // Step 6: Add another property named placesLived with a value of an empty array
-
 // Step 7: Inside of the empty array above, add a new object with two properties: place and length and values of an empty string
-
 // Step 8: For each property, add appropriate values as strings
-
 // Step 9: Add additional objects with the same properties for each place you've lived
+    placesLived: [
+        {
+            "place": "Florida, Uruguay",
+            "length":"22 years"
+        },
+
+        {
+            "place": "Montevideo, Uruguay",
+            "length":"5 years"
+        },
+    ]
+}
+
 
 
 /* OUTPUT */
